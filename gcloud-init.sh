e6c0ee9b2c5c34f07d178bc9f2f1eea281278372
Damon Reed <dreed@nomadia.org>
2023-11-16 07:34:28 -0600

    Update gcloud init script

--- gcloud-init.sh
+++ gcloud-init.sh
@@ -7,7 +7,14 @@ gcloud config set accessibility/screen_reader false
 
 #Set Important Vars
 export PROJECT_ID=$(gcloud config get-value project)
-export REGION="us-central1"
-export ZONE="us-central1-a"
+export REGION="us-east1"
+export ZONE="us-east1-d"
 gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
+
+# Show starting resources
+gcloud compute networks list
+gcloud compute networks subnets list
+gcloud compute firewall-rules list
+gcloud compute instances list
+
