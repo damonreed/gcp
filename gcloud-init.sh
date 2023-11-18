39bfaa8fc94910808ea78654da305e352717827e
Damon Reed <dreed@nomadia.org>
2023-11-18 13:03:02 -0600

    Update gcloud init to ZONE="us-central1-a"

--- gcloud-init.sh
+++ gcloud-init.sh
@@ -7,8 +7,8 @@ gcloud config set accessibility/screen_reader false
 
 #Set Important Vars
 export PROJECT_ID=$(gcloud config get-value project)
-export REGION="us-east1"
-export ZONE="us-east1-d"
+export REGION="us-central1"
+export ZONE="us-central1-a"
 gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
 
